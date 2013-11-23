ae0acbdfb6eaf6bbe8eb87456b2ebbeb42f17b51
Artem Bochkarev <artem.bochkarev@gmail.com>
2013-11-23 22:59:53 +0400

    Short tables CPU implementation added

--- Source/Genetic/CAutomatShortTables.cpp
+++ Source/Genetic/CAutomatShortTables.cpp
@@ -1,34 +1,202 @@
 #include "stdafx.h"
 #include "CAutomatShortTables.h"
+#include "Tools\binFunc.hpp"
 
-/*size_t CAutomatShortTables::countIndex(int* mas) const
+size_t CAutomatShortTables::countIndex(std::vector<INPUT_TYPE>* mas) const
 {
-	int* mask = buffer + currentState * stateSize;
+	COUNTERS_TYPE* currentMask = buffer + currentState * (tableSize + maskSize);
 	int index = 0, k = 1, j = 0;
-	for (int i = 0; i < INPUT_PARAMS_COUNT; ++i)
+	for (size_t i = 0; i < INPUT_PARAMS_COUNT; ++i)
 	{
-		if (mask[i])
+		if (currentMask[i])
 		{
-			index += k*mas[j++];
+			index += k*mas->at(j++);
 			k *= 2;
 		}
 	}
 	return index;
 }
 
-char CAutomatShortTables::getNextState(char currentState, int* input) const
+COUNTERS_TYPE CAutomatShortTables::getNextState(COUNTERS_TYPE currentState, std::vector<INPUT_TYPE>* input) const
 {
 	size_t index = countIndex(input);
-	char * ptrStates = buffer + currentState * 2 * stateSize;
-	char state = *(ptrStates + index);
+	COUNTERS_TYPE * currentTable = buffer + currentState * (tableSize + maskSize) + maskSize;
+	COUNTERS_TYPE state = *(currentTable + recordSize*index + stateShift);
 	return state;
 }
 
-char CAutomatShortTables::getAction(char currentState, int* input) const
+COUNTERS_TYPE CAutomatShortTables::getAction(COUNTERS_TYPE currentState, std::vector<INPUT_TYPE>* input) const
 {
 	size_t index = countIndex(input);
-	char * ptrStates = buffer + currentState * 2 * stateSize;
-	char * ptrActions = ptrStates + stateSize;
-	char action = *(ptrActions + index);
+	COUNTERS_TYPE * currentTable = buffer + currentState * (tableSize + maskSize) + maskSize;
+	COUNTERS_TYPE action = *(currentTable + recordSize*index + actionShift);
 	return action;
-}*/
+}
+
+void CAutomatShortTables::mutate(CRandom* rand)
+{
+	for (size_t currentState = 0; currentState < statesCount; ++currentState)
+	{
+		COUNTERS_TYPE* currentMask = buffer + currentState * (tableSize + maskSize);
+		mutateMask(currentMask, rand);
+		COUNTERS_TYPE * currentTable = buffer + currentState * (tableSize + maskSize) + maskSize;
+		mutateTable(currentTable, rand);
+	}
+
+	if ((rand->nextUINT() & 255) < 30)
+		startState = rand->nextUINT()%statesCount;
+}
+
+void CAutomatShortTables::mutateTable(COUNTERS_TYPE* currentTable, CRandom* rand)
+{
+	for (size_t i = 0; i < tableSize; i += recordSize)
+	{
+		uint a = rand->nextUINT() & 255;
+		if (a == 100)
+			currentTable[i + stateShift] = states->randomState(rand);
+		if ((a >= 100) && (a <= 104))
+			currentTable[i + actionShift] = actions->randomAction(rand);
+	}
+}
+
+void CAutomatShortTables::mutateMask(COUNTERS_TYPE* currentMask, CRandom* rand)
+{
+	uint a = rand->nextUINT() & 255;
+	if ((a > 31) && (a < 38))
+		//small probaility to change mask
+	{
+		uint alreadyEnabledCnt = 0;
+		for (size_t i = 0; i < INPUT_PARAMS_COUNT; ++i)
+		if (currentMask[i])
+			++alreadyEnabledCnt;
+		uint toDel = rand->nextUINT()%INPUT_PARAMS_COUNT;
+		uint toAdd = rand->nextUINT()%INPUT_PARAMS_COUNT;
+		if (alreadyEnabledCnt == MAX_PARAMETERS)
+		{
+			if (currentMask[toDel])
+			{
+				currentMask[toDel] = 0;
+				currentMask[toAdd] = 1;
+			}
+		}
+		else
+		{
+			currentMask[toDel] = 0;
+			currentMask[toAdd] = 1;
+		}
+	}
+}
+
+void CAutomatShortTables::crossover(const CAutomat<COUNTERS_TYPE, INPUT_TYPE>* mother, const CAutomat<COUNTERS_TYPE, INPUT_TYPE>* father, CRandom* rand)
+{
+	const CAutomatShortTables* motherPtr = static_cast<const CAutomatShortTables*>(mother);
+	const CAutomatShortTables* fatherPtr = static_cast<const CAutomatShortTables*>(father);
+
+	for (size_t currentState = 0; currentState < statesCount; ++currentState)
+	{
+		COUNTERS_TYPE* motherMask = motherPtr->buffer + currentState * (tableSize + maskSize);
+		COUNTERS_TYPE* fatherMask = fatherPtr->buffer + currentState * (tableSize + maskSize);
+		COUNTERS_TYPE* childMask = buffer + currentState * (tableSize + maskSize);
+
+		crossMasks(childMask, motherMask, fatherMask, rand);
+		crossTables(childMask, motherMask, fatherMask, rand);
+	}
+}
+
+void CAutomatShortTables::crossMasks(COUNTERS_TYPE* childMask, const COUNTERS_TYPE* motherMask, const COUNTERS_TYPE* fatherMask, CRandom* rand)
+{
+	size_t bothEnabled = 0, oneEnabled = 0, enabled = 0;
+	for (size_t i = 0; i < INPUT_PARAMS_COUNT; ++i)
+	{
+		if (motherMask[i] && fatherMask[i])
+		{
+			childMask[i] = 1;
+			bothEnabled++;
+			enabled++;
+		}
+		else if (motherMask[i] || fatherMask[i])
+			oneEnabled++;
+	}
+
+	for (size_t i = 0; i < INPUT_PARAMS_COUNT; ++i)
+	{
+		if (childMask[i] == 0)
+		{
+			if (motherMask[i] || fatherMask[i])
+			{
+				size_t res = ((rand->nextUINT() & 255) < (255 / (MAX_PARAMETERS - bothEnabled + 1))) ? 1 : 0;
+				if (res && (enabled < MAX_PARAMETERS))
+				{
+					++enabled;
+					childMask[i] = 1;
+				}
+			}
+		}
+	}
+}
+
+void CAutomatShortTables::crossTables(COUNTERS_TYPE* childMask, const COUNTERS_TYPE* motherMask, const COUNTERS_TYPE* fatherMask, CRandom* rand)
+{
+	size_t myMas[MAX_PARAMETERS], j = 0;
+	for (size_t i = 0; i < INPUT_PARAMS_COUNT; ++i)
+	{
+		if (childMask[i])
+			myMas[j++] = i;
+	}
+
+	size_t toMother[MAX_PARAMETERS], toFather[MAX_PARAMETERS];
+	toParent(toMother, myMas, motherMask);
+	toParent(toFather, myMas, fatherMask);
+
+	size_t maxIndex = Tools::twoPow(MAX_PARAMETERS);
+	for (size_t index = 0; index < maxIndex; ++index)
+	{
+		size_t motherIndex = createParentIndex(toMother, myMas, index, rand);
+		size_t fatherIndex = createParentIndex(toFather, myMas, index, rand);
+		COUNTERS_TYPE * childTable = childMask + maskSize;
+		const COUNTERS_TYPE * motherTable = motherMask + maskSize;
+		const COUNTERS_TYPE * fatherTable = fatherMask + maskSize;
+		if ((rand->nextUINT() & 255) > 127)
+			memcpy(childTable + index*recordSize, motherTable + motherIndex*recordSize, recordSize*sizeof(COUNTERS_TYPE));
+		else
+			memcpy(childTable + index*recordSize, fatherTable + fatherIndex*recordSize, recordSize*sizeof(COUNTERS_TYPE));
+	}
+}
+
+size_t CAutomatShortTables::createParentIndex(const size_t* toParent, const size_t* myArray, size_t index, CRandom* rand)
+{
+	size_t maxIndex = Tools::twoPow(MAX_PARAMETERS);
+	size_t parentIndex = rand->nextUINT()&(maxIndex - 1);
+	for (size_t i = 0; i < MAX_PARAMETERS; ++i)
+	{
+		//change toGetBit
+		size_t myBit = Tools::checkBit(index, myArray[i]);
+		if (toParent[i])
+		{
+			//change to SteBit
+			if (myBit)
+				Tools::setBit(parentIndex, toParent[i] - 1);
+			else
+				Tools::clearBit(parentIndex, toParent[i] - 1);
+		}
+	}
+	return parentIndex;
+}
+
+void CAutomatShortTables::toParent(size_t* toMother, const size_t* myMas, const COUNTERS_TYPE* motherMask)
+{
+	for (size_t i = 0; i < MAX_PARAMETERS; ++i)
+		toMother[i] = 0;
+	size_t j = 1, k = 0;
+	for (size_t i = 0; i < INPUT_PARAMS_COUNT; ++i)
+	{
+		if (i > myMas[k] && k < MAX_PARAMETERS)
+			++k;
+		if (motherMask[i])
+		{
+			if (myMas[k] == i)
+				toMother[k] = j;
+			++j;
+		}
+	}
+}
--- Source/Genetic/CAutomatShortTables.h
+++ Source/Genetic/CAutomatShortTables.h
@@ -3,22 +3,28 @@
 
 class CAutomatShortTables;
 typedef boost::shared_ptr<CAutomatShortTables> CAutomatShortTablesPtr;
+static const size_t recordSize = 2;
+static const size_t stateShift = 0;
+static const size_t actionShift = 1;
+static const size_t MAX_PARAMETERS = 3;
+
 
 class CAutomatShortTables : public CAutomat<COUNTERS_TYPE, INPUT_TYPE>
 {
 public:
-	CAutomatShortTables(CStateContainer<COUNTERS_TYPE>* states, CActionContainer<COUNTERS_TYPE>* actions, size_t stateSize = 16);
+	CAutomatShortTables(CStateContainer<COUNTERS_TYPE>* states, CActionContainer<COUNTERS_TYPE>* actions, size_t stateSize);
     CAutomatShortTables( const CAutomatShortTables& automat );
     CAutomatShortTables& operator = ( const CAutomatShortTables& automat );
     virtual void generateRandom( CRandom* rand );
-    virtual char getNextState( char currentState, std::vector<int>* input );
-    virtual char getNextState( char currentState, int* input );
-    virtual char getStartState() const;
-    virtual char getAction( char currentState, std::vector<int>* input );
-    virtual char getAction( char currentState, int* input );
-    virtual size_t countIndex( std::vector<int>* input ) const;
-    virtual size_t countIndex( int* input ) const;
-    virtual void mutate( CRandom* rand );
+	virtual COUNTERS_TYPE getNextState(COUNTERS_TYPE currentState, std::vector<INPUT_TYPE>* input) const;
+	virtual COUNTERS_TYPE getAction(COUNTERS_TYPE currentState, std::vector<INPUT_TYPE>* input) const;
+	virtual COUNTERS_TYPE getStartState() const;
+
+	//virtual COUNTERS_TYPE getNextState(COUNTERS_TYPE currentState, INPUT_TYPE* input);
+	//virtual COUNTERS_TYPE getAction(COUNTERS_TYPE currentState, INPUT_TYPE* input);	
+	//virtual size_t countIndex(INPUT_TYPE* input) const;
+    
+	virtual void mutate( CRandom* rand );
     virtual void crossover( const CAutomat* mother, const CAutomat* father, CRandom* rand );
 
 	static void fillRandom(CStateContainer<COUNTERS_TYPE>* states, CActionContainer<COUNTERS_TYPE>* actions, char* buff,
@@ -29,10 +35,19 @@ public:
 		CActionContainer<COUNTERS_TYPE>* actions, char* buf, size_t stateSize = 16);
 
     virtual ~CAutomatShortTables();
+protected:
+	virtual size_t countIndex(std::vector<INPUT_TYPE>* input) const;
+	static void toParent(size_t* toMother, const size_t* myMas, const COUNTERS_TYPE* motherMask);
+	static void crossMasks(COUNTERS_TYPE* childMask, const COUNTERS_TYPE* motherMask, const COUNTERS_TYPE* fatherMask, CRandom* rand);
+	static void crossTables(COUNTERS_TYPE* childMask, const COUNTERS_TYPE* motherMask, const COUNTERS_TYPE* fatherMask, CRandom* rand);
+	static void mutateMask(COUNTERS_TYPE* currentMask, CRandom* rand);
+	void mutateTable(COUNTERS_TYPE* currentTable, CRandom* rand);
+	static size_t createParentIndex(const size_t* toParent, const size_t* myArray, size_t index, CRandom* rand);
 private:
     CAutomatShortTables() {};
 	CStateContainer<COUNTERS_TYPE>* states;
 	CActionContainer<COUNTERS_TYPE>* actions;
-    size_t statesCount, stateSize, startState, currentState;
+	static size_t statesCount, stateSize, maskSize, tableSize;
+	size_t startState, currentState;
 	COUNTERS_TYPE * buffer;
 };
